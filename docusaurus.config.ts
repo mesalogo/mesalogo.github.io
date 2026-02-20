77088ad910634ddf52dd822a03aaa2009804c662
lofyer <lofyer@gmail.com>
2026-02-20 14:39:51 +0800

    add GitHub Pages deploy workflow and fix projectName

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mesalogo', // Usually your GitHub org/user name.
-  projectName: 'abm-llm-v2', // Usually your repo name.
+  projectName: 'abm-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
